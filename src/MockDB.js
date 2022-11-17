f478fa4109e778bc4d14a9d900e1e33dd8ac6646
GreatEki <ekeneidiagbor@gmail.com>
2022-11-17 13:10:03 +0100

    chore: seeded friends in mock db

--- src/MockDB.js
+++ src/MockDB.js
@@ -5,6 +5,22 @@ const UserList = [
     username: "john",
     age: 20,
     nationality: "Canada",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "Brazil",
+      },
+      {
+        id: 3,
+        name: "Sarah",
+        username: "Cameron",
+        age: 20,
+        nationality: "United States",
+      },
+    ],
   },
   {
     id: 2,
@@ -12,6 +28,15 @@ const UserList = [
     username: "PedroTech",
     age: 20,
     nationality: "Brazil",
+    friends: [
+      {
+        id: 1,
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "Canada",
+      },
+    ],
   },
   {
     id: 3,
@@ -19,6 +44,22 @@ const UserList = [
     username: "Cameron",
     age: 20,
     nationality: "United States",
+  },
+  {
+    id: 34,
+    name: "Kelly",
+    username: "kelly2021",
+    age: 19,
+    nationality: "United States",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "Brazil",
+      },
+    ],
   },
 ];
 
